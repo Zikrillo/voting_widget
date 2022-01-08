9ce8b1449901e6b5aeef02dbd725e89404e74edd
Zikrillo Abbosov <zikrillo5018@gmail.com>
2022-01-08 05:53:10 +0500

    Added element items css/html elements

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+let score = 1;
+let score_1 =1;
+let per = document.querySelector(".per");
+let per_s = document.querySelector(".per_s");
+let coloumn = document.querySelectorAll(".coloumn")[0];
+let coloumn_s = document.querySelectorAll(".coloumn")[1];
+coloumn.addEventListener('click', ()=>{
+    alert("hello");
+});
+
+
