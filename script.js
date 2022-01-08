bda5882003fadeea0c6b83d8baef086a15051ec0
Zikrillo Abbosov <zikrillo5018@gmail.com>
2022-01-08 07:30:16 +0500

    Added script for calculating percentage

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 "use strict";
-let score = 1;
-let score_1 =1;
+let score = 2;
+let score_1 = 1;
 let per = document.querySelector(".per");
 let per_s = document.querySelector(".per_s");
 let coloumn = document.querySelectorAll(".coloumn")[0];
 let coloumn_s = document.querySelectorAll(".coloumn")[1];
+
+
 coloumn.addEventListener('click', ()=>{
-    alert("hello");
+    score++;
+    per.style.width = `${Math.round(score/(score + score_1)*100)}%`;
+    per_s.style.width = `${100-Math.round(score/(score + score_1)*100)}%`;
+});
+
+
+coloumn_s.addEventListener('click', ()=>{
+    score_1++;
+    per.style.width = `${Math.round(score/(score + score_1)*100)}%`;
+    per_s.style.width = `${100-Math.round(score/(score + score_1)*100)}%`
 });
 
 
